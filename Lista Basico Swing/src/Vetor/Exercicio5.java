87412a159528b15a15a1c3e0b54db521205dd612
Pedro Henrique GOdri <pedrogodri@gmail.com>
2023-02-12 14:49:10 -0300

    Exercicio de Vetor finalizado

---
+++ Lista Basico Swing/src/Vetor/Exercicio5.java
@@ -0,0 +1,24 @@
+package Vetor;
+
+import javax.swing.JOptionPane;
+
+public class Exercicio5 {
+
+	public static void main(String[] args) {
+		Integer maior = Integer.MIN_VALUE; 
+		Integer menor = Integer.MAX_VALUE;
+		Integer[] vetor = new Integer[10];
+		for (int i = 0; i < vetor.length; i++) {
+			vetor[i] = Integer.valueOf(JOptionPane.showInputDialog(null, "Digite um valor: "));
+			if(vetor[i] > maior) {
+				maior = vetor[i];
+			}
+			if(vetor[i] < menor) {
+				menor = vetor[i];
+			}
+			
+		}
+		JOptionPane.showMessageDialog(null, "Maior = " + maior + "\nMenor = " + menor);
+	}
+
+}
